629377d1f27d9a2abd63db7dcd16781fa8ec3254
MaherLawand <maher.lawand9@gmail.com>
2022-11-12 15:59:11 +0200

    Create Bee.java

---
+++ Bee.java
@@ -0,0 +1,45 @@
+
+public class Bee {
+	private String TypeOfBee;
+	private int EfficiencyOfBee;
+	private int LifeSpan;
+	private int EstimatesNbOfBees;
+	public Bee() {
+		TypeOfBee="";
+		EfficiencyOfBee=0;
+		LifeSpan=0;
+		EstimatesNbOfBees=0;
+	}
+	public Bee(String typeOfBee, int efficiencyOfBee, int lifeSpan, int estimatesNbOfBees) {
+		super();
+		TypeOfBee = typeOfBee;
+		EfficiencyOfBee = efficiencyOfBee;
+		LifeSpan = lifeSpan;
+		EstimatesNbOfBees = estimatesNbOfBees;
+	}
+	public String getTypeOfBee() {
+		return TypeOfBee;
+	}
+	public void setTypeOfBee(String typeOfBee) {
+		TypeOfBee = typeOfBee;
+	}
+	public int getEfficiencyOfBee() {
+		return EfficiencyOfBee;
+	}
+	public void setEfficiencyOfBee(int efficiencyOfBee) {
+		EfficiencyOfBee = efficiencyOfBee;
+	}
+	public int getLifeSpan() {
+		return LifeSpan;
+	}
+	public void setLifeSpan(int lifeSpan) {
+		LifeSpan = lifeSpan;
+	}
+	public int getEstimatesNbOfBees() {
+		return EstimatesNbOfBees;
+	}
+	public void setEstimatesNbOfBees(int estimatesNbOfBees) {
+		EstimatesNbOfBees = estimatesNbOfBees;
+	}
+	
+}
