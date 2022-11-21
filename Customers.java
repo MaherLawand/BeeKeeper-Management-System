498803a8edf3323588e883abeb797bf1c297c5bd
MaherLawand <maher.lawand9@gmail.com>
2022-11-21 20:11:05 +0200

    Customers

--- Customers.java
+++ Customers.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Comparator;
 
 public class Customers {
+	HashSet <Customers> ActiveCustomers = new HashSet<Customers>();
+	HashSet <Customers> BannedCustomers = new HashSet<Customers>();
 	private String FName;
 	private String LName;
 	private String email;
@@ -51,20 +57,77 @@ public class Customers {
 		this.address = address;
 		PhoneNumber = phoneNumber;
 	}
-	public void addCustomer() {
-		
+	public void addCustomer(Customers s) {
+		if(ActiveCustomers.contains(s)){
+			System.out.println("Already Registered!");
+		}else if(BannedCustomers.contains(s)){
+			System.out.println("Customer Banned!");
+		}else{
+			ActiveCustomers.add(s);
+		}
 	}
-	public Customers banCustomer(String email, int phonenumber) {
-		
+	public Customers banCustomer(Customers s) {
+		Customers banned=s;
+		if(!BannedCustomers.contains(s) && ActiveCustomers.contains(s)){
+			BannedCustomers.add(s);
+			ActiveCustomers.remove(s);
+		}else if(!ActiveCustomers.contains(s)){
+			System.out.println("Customer Doesnt Exist!");
+		}else{
+			System.out.println("Customer Already Banned!");
+		}
+		return banned;
 	}
-	public Customers SearchCustomerbyEmailandPhoneNumber(String email,int phonenumber) {
-		
-		
-	}
+	public Customers SearchCustomerbyEmail(String email) {
+		Customers[] customers = ActiveCustomers.toArray(new Customers[ActiveCustomers.size()]);
+		// for(int i=0;i<customers.length;i++){
+		// 	System.out.println(customers[i].email);
+		// }
+		Arrays.sort(customers, new Comparator<Customers>() {
+			@Override
+			public int compare(Customers o1, Customers o2) {
+				return o1.getEmail().compareTo(o2.getEmail());
+			}
+		});
+		int l = 0, r = customers.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (customers[m].getEmail().compareTo(email) < 0) {
+				l = m + 1; 
+			} else if (customers[m].getEmail().compareTo(email) > 0) {
+				r = m - 1;
+			} 
+			else{
+				return customers[m];
+			}
+    }
+	return null;
+}
+	
 	public void ListCustomers() {
-		
+		for (Customers s : ActiveCustomers) {
+			System.out.println("First Name: " + s.getFName());
+			System.out.println("Last Name: " + s.getLName());
+			System.out.println("Email: " + s.getEmail());
+			System.out.println("Address: " + s.getAddress());
+			System.out.println("Phone Number: " + s.getPhoneNumber());
+			System.out.println("-------------------------------------");
+			
+		}
 	}
 	public void ListBannedCustomers() {
-		
+		for (Customers s : BannedCustomers) {
+			System.out.println("First Name: " + s.getFName());
+			System.out.println("Last Name: " + s.getLName());
+			System.out.println("Email: " + s.getEmail());
+			System.out.println("Address: " + s.getAddress());
+			System.out.println("Phone Number: " + s.getPhoneNumber());
+			System.out.println("-------------------------------------");
+			
+		}
 	}
+
+
+	
 }
+
--- mmmain.java
+++ mmmain.java
@@ -8,29 +8,47 @@ public class mmmain {
 //        h1.addBee("Ohio", 15, 100, 217);
 //        h2.addBee("Ohio", 15, 100, 217);
 //        h.addBee("Ohio", 15, 100, 217);
-//        
-//      
+       
+     
 //        Apiary f = new Apiary("nigroo", 69, 30.3325 , "Da7ye" );
-//        
+       
 //        f.addHiveToApiary(h);
 //        f.addHiveToApiary(h1);
 //        f.addHiveToApiary(h2);
 //        f.ListHivesInApiary();
 //        System.out.println(f.FindHiveBYSerialNBfromApiary(3));
-//       
-//        
+      
+       
 //        f.sortHivesInApiaryByQueenStatus();
 //        f.ListHivesInApiary();
-//        
-//        
+       
+       
 //        System.out.println(f.FindHiveBYSerialNBfromApiary(3));
-//        
+       
 //        f.removeHiveBYSerialNBfromApiary(1);
 //        f.ListHivesInApiary();
-//        
-        Stock s = new Stock();
-        s.DisplayStock();
+       
         
         
+//     }
+// }
+Customers all = new Customers();
+Customers s1 = new Customers("Maher","Lawand","maher.lawand9@gmail.com","test",70300981);
+Customers s3 = new Customers("arij","fg","fds@gmail.com","sd",546546);
+Customers s4 = new Customers("arij","fg","fdgs@gmail.com","sd",546546);
+
+all.addCustomer(s1);
+all.addCustomer(s3);
+all.addCustomer(s4);
+all.banCustomer(s4);
+
+Customers email=all.SearchCustomerbyEmail("mdfs9@gmail.com");
+if(email!=null){
+    System.out.println(email.getEmail());
+}else{
+    System.out.println("null");
+}
+all.ListBannedCustomers();
+
     }
 }
